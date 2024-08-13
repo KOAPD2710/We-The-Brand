2551002ee99ff89624a8f2c59dfd554c39f0f6d5
KOAPD2710 <khoaphan27102000@gmail.com>
2024-08-13 16:17:45 +0700

    Update Pitch Sc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-// import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    // output: 'server',
-    // adapter: vercel({
-    //     webAnalytics: { enabled: true }
-    // }),
+    output: 'server',
+    adapter: vercel({
+        webAnalytics: { enabled: true }
+    }),
     integrations: [react({
         experimentalReactChildren: true
     })],
