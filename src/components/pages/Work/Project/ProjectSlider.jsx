dc4729a590deedf18b455a3311c3e8f4227b0e55
KOAPD2710 <khoaphan27102000@gmail.com>
2024-08-24 17:38:26 +0700

    Update Work Page

--- src/components/pages/Work/Project/ProjectSlider.jsx
+++ src/components/pages/Work/Project/ProjectSlider.jsx
@@ -1,5 +1,5 @@
 import { getLenis } from '@/components/core/lenis';
-import { lerp, parseRem, sawtooth } from '@/js/utils';
+import { lerp, parseRem, parseToRem, sawtooth } from '@/js/utils';
 import { useKeenSlider } from 'keen-slider/react';
 import { useState, useEffect } from 'react';
 
@@ -15,7 +15,7 @@ const isInview = (el, gap) => {
 
 const ProjectSlider = ({ allProject, ...props }) => {
     let currScroll = 0;
-
+    let time = 0
 
     useEffect(() => {
         const lenis = getLenis(true);
@@ -34,30 +34,42 @@ const ProjectSlider = ({ allProject, ...props }) => {
             allThumbs: Array.from(document.querySelectorAll('.work-project-thumb-item')),
             wrapper: document.querySelector('.work-project-thumb')
         }
+
         let wrapperWidth = target.wrapper.clientWidth
         document.querySelector('.work-project').style.height = `${wrapperWidth * 4}px`
+
         let raf;
         let initPos = Array.from(target.allThumbs, (el) => 0);
+        let expandWidth = parseRem(10 * parseFloat(window.getComputedStyle(target.wrapper).getPropertyValue('--expand-w')))
 
-        function translatethumb() {
+        function translateThumb() {
+            time += 1 / 2
+
             target.allThumbs.forEach((el, idx) => {
-                const { right } = el.getBoundingClientRect()
+                let targetImg = el.querySelector('.work-project-thumb-item-img img')
+                const { left, width, right } = el.getBoundingClientRect()
                 if (right < 0) {
-                    let multiply = Math.floor(currScroll / wrapperWidth) + (idx === (target.allThumbs.length - 1) ? 0 : 1)
+                    let multiply = Math.floor((currScroll + time) / wrapperWidth) + (idx === (target.allThumbs.length - 1) ? 0 : 1)
                     initPos[idx] = wrapperWidth * multiply
                 }
                 if (right > wrapperWidth) {
-                    let multiply = Math.floor(currScroll / wrapperWidth)
+                    let multiply = Math.floor((currScroll + time) / wrapperWidth)
                     initPos[idx] = wrapperWidth * multiply
-
                 }
+                let normalizeOffset = (right / (window.innerWidth + width) - .5) * 2
 
-                el.style.transform = `translateX(${-currScroll + initPos[idx]}px)`
+                if (idx == 0) {
+                    console.log(normalizeOffset);
+                }
+                targetImg.style.transform = `translateX(${-normalizeOffset * expandWidth / 2}px)`
+                el.style.transform = `translateX(${-currScroll - time + initPos[idx]}px)`
+                // el.style.transform = `translateX(${-860}px)`
+                // el.style.transform = `translateX(${1745}px)`
             });
-            raf = window.requestAnimationFrame(translatethumb);
+            raf = window.requestAnimationFrame(translateThumb);
         }
 
-        raf = window.requestAnimationFrame(translatethumb);
+        raf = window.requestAnimationFrame(translateThumb);
 
         return () => {
             window.cancelAnimationFrame(raf);
@@ -69,9 +81,11 @@ const ProjectSlider = ({ allProject, ...props }) => {
         <div className="work-project-thumb">
             {allProject.map((proj, idx) => (
                 <div className="work-project-thumb-item" key={proj.name + idx}>
+                    <div className="h1 name">
+                        {proj.name}-{idx}
+                    </div>
                     <div className="work-project-thumb-item-img">
                         <img src={proj.thumb.src} alt="" className='img img-fill' />
-                        <span>{idx}</span>
                     </div>
                 </div>
             ))}
