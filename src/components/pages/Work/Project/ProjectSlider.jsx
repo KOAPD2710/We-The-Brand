d7888032b3579804a12f84e2342da00ecdeb0ba5
KOAPD2710 <khoaphan27102000@gmail.com>
2024-08-24 15:53:40 +0700

    Update Work Page

--- src/components/pages/Work/Project/ProjectSlider.jsx
+++ src/components/pages/Work/Project/ProjectSlider.jsx
@@ -21,7 +21,7 @@ const ProjectSlider = ({ allProject, ...props }) => {
         const lenis = getLenis(true);
 
         lenis.on('scroll', (e) => {
-            currScroll = lerp(currScroll, currScroll + e.direction * 2 * Math.abs(e.velocity), .05);
+            currScroll = lerp(currScroll, currScroll + e.direction * 3 * Math.abs(e.velocity), .05);
         });
 
         return () => {
@@ -35,6 +35,7 @@ const ProjectSlider = ({ allProject, ...props }) => {
             wrapper: document.querySelector('.work-project-thumb')
         }
         let wrapperWidth = target.wrapper.clientWidth
+        document.querySelector('.work-project').style.height = `${wrapperWidth * 4}px`
         let raf;
         let initPos = Array.from(target.allThumbs, (el) => 0);
 
